41c2c3d791a2bc59e1882109c37399036583dfa8
Philippe Gross <p.gross@4teamwork.ch>
2010-04-29 08:25:49 +0000

    added first experimental release

---
+++ transmogrify/dexterity/dexterityupdater.py
@@ -0,0 +1,78 @@
+from zope import event
+from zope.interface import classProvides, implements
+from collective.transmogrifier.interfaces import ISectionBlueprint, ISection
+from collective.transmogrifier.utils import Matcher, defaultKeys
+
+from Products.Archetypes.interfaces import IBaseObject
+from Products.Archetypes.event import ObjectInitializedEvent
+from Products.Archetypes.event import ObjectEditedEvent
+from plone.dexterity.utils import iterSchemata
+from zope.schema import getFieldsInOrder
+
+class DexterityUpdateSection(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+    
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.name = name
+        
+        if 'path-key' in options:
+            pathkeys = options['paht-key'].splitlines()
+        else:
+            pathkeys = defaultKeys(options['blueprint'], name, 'path')
+        self.pathkey = Matcher(*pathkeys)
+
+
+    def __iter__(self):
+        for item in self.previous:
+            pathkey = self.pathkey(*item.keys())[0]
+            
+            if not pathkey:         # not enough info
+                yield item; continue
+
+            path = item[pathkey]
+
+            obj = self.context.unrestrictedTraverse(path.lstrip('/'), None)
+            if obj is None:         # path doesn't exist
+                yield item; continue
+
+            #get all fields for this obj
+            fields = {}
+            for schemata in iterSchemata(obj):
+                for name, field in getFieldsInOrder(schemata):
+                    fields[name] = field
+            for k,v in item.iteritems():
+                if k.startswith('_'):
+                    continue
+                field = fields[k]
+                #TODO implements different types (boolean, int, list, file ect)
+                field.set(field.interface(obj), v.decode('iso-8859-1'))
+                
+
+            if IBaseObject.providedBy(obj):
+                changed = False
+                is_new_object = obj.checkCreationFlag()
+                for k,v in item.iteritems():
+                    if k.startswith('_'):
+                        continue
+                    field = obj.getField(k)
+                    if field is None:
+                        continue
+                    field.set(obj, v)
+                    changed = True
+                obj.unmarkCreationFlag()
+
+                if is_new_object:
+                    event.notify(ObjectInitializedEvent(obj))
+                    obj.at_post_create_script()
+                elif changed:
+                    event.notify(ObjectEditedEvent(obj))
+                    obj.at_post_edit_script()
+
+            yield item
+
+
+
+        
