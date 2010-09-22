b4afd0bd63fd0416108c609dc09d9bbd57153341
Philippe Gross <p.gross@4teamwork.ch>
2010-09-22 12:33:15 +0000

    Refactoring Dexterityupdater:  - iterate over all fields in the schema an not over the given items, so its possible to set the default or the missing value

--- transmogrify/dexterity/dexterityupdater.py
+++ transmogrify/dexterity/dexterityupdater.py
@@ -1,14 +1,11 @@
-from zope import event
+from zope.component import queryMultiAdapter
 from zope.interface import classProvides, implements
 from collective.transmogrifier.interfaces import ISectionBlueprint, ISection
 from collective.transmogrifier.utils import Matcher, defaultKeys
-
-from Products.Archetypes.interfaces import IBaseObject
-from Products.Archetypes.event import ObjectInitializedEvent
-from Products.Archetypes.event import ObjectEditedEvent
+from z3c.form.interfaces import IValue
 from plone.dexterity.utils import iterSchemata
 from zope.schema import getFieldsInOrder
-from zope.schema.interfaces import IList, IDate
+from zope.schema.interfaces import IList, IDate, IInt, IBool
 from datetime import datetime
 
 
@@ -16,86 +13,85 @@ class DexterityUpdateSection(object):
 
     classProvides(ISectionBlueprint)
     implements(ISection)
-    
+
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.context = transmogrifier.context
         self.name = name
-        
+
         if 'path-key' in options:
             pathkeys = options['paht-key'].splitlines()
         else:
             pathkeys = defaultKeys(options['blueprint'], name, 'path')
         self.pathkey = Matcher(*pathkeys)
 
-
     def __iter__(self):
         for item in self.previous:
             pathkey = self.pathkey(*item.keys())[0]
-            
+
             if not pathkey:         # not enough info
-                yield item; continue
+                yield item
+                continue
 
             path = item[pathkey]
 
-            obj = self.context.unrestrictedTraverse(path.encode().lstrip('/'), None)
-            if obj is None:         # path doesn't exist
-                yield item; continue
+            obj = self.context.unrestrictedTraverse(
+                path.encode().lstrip('/'), None)
+
+            # path doesn't exist
+            if obj is None:
+                yield item
+                continue
 
             #get all fields for this obj
-            fields = {}
             for schemata in iterSchemata(obj):
                 for name, field in getFieldsInOrder(schemata):
-                    fields[name] = field
-            for k,v in item.iteritems():
-                if k.startswith('_'):
-                    continue
-                if k not in fields:
-                    continue
-                field = fields[k]
 
-                #boolean field
-                if v is None or v == '':
-                    continue
-                if v.lower()=='true':
-                    field.set(field.interface(obj), True)
-                    continue
-                elif v.lower()=='false':
-                    field.set(field.interface(obj), False)
-                    continue
-                # listfield
-                if IList.providedBy(field):
-                    v = filter(lambda p:not not p,
-                               [p.strip() for p in v.split(',')])
-                    field.set(field.interface(obj), v)
-                # datefield
-                elif IDate.providedBy(field):
-                    v = datetime.strptime(v,"%d.%m.%Y")
-                    field.set(field.interface(obj), v)
-                # integer check
-                elif v.isdigit():
-                    field.set(field.interface(obj), int(v))
-                else:
-                    field.set(field.interface(obj), v)
+                    #setting value from the blueprint cue
+                    value = item.get(name)
 
-            if IBaseObject.providedBy(obj):
-                changed = False
-                is_new_object = obj.checkCreationFlag()
-                for k,v in item.iteritems():
-                    if k.startswith('_'):
-                        continue
-                    field = obj.getField(k)
-                    if field is None:
-                        continue
-                    field.set(obj, v)
-                    changed = True
-                obj.unmarkCreationFlag()
+                    if value:
+                        if IDate.providedBy(field):
+                            v = datetime.strptime(value, "%d.%m.%Y")
+                            field.set(field.interface(obj), value)
 
-                if is_new_object:
-                    event.notify(ObjectInitializedEvent(obj))
-                    obj.at_post_create_script()
-                elif changed:
-                    event.notify(ObjectEditedEvent(obj))
-                    obj.at_post_edit_script()
+                        elif IBool.providedBy(field):
+                            if value.lower()=='true':
+                                field.set(field.interface(obj), True)
+                            else:
+                                field.set(field.interface(obj), False)
 
+                        elif IList.providedBy(field):
+                            if IList.providedBy(field):
+                                v = filter(
+                                    lambda p: not not p,
+                                    [p.strip() for p in value.split(';')])
+                                field.set(field.interface(obj), v)
+
+                        elif IInt.providedBy(field):
+                            field.set(field.interface(obj), int(value))
+                        else:
+                            field.set(field.interface(obj), value)
+                    else:
+                        # No value is given from the blueprint cue,
+                        # so we try to set the default value
+                        # otherwise we set the missing value
+
+                        default = queryMultiAdapter((
+                                obj,
+                                obj.REQUEST, # request
+                                None, # form
+                                field,
+                                None, # Widget
+                                ), IValue, name='default')
+                        if default!=None:
+                            default = default.get()
+                        if default==None:
+                            default = getattr(field, 'default', None)
+                        if default==None:
+                            try:
+                                default = field.missing_value
+                            except:
+                                pass
+                        field.set(field.interface(obj), default)
             yield item
