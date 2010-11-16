dbf9284c6d33482b35304e5c6cebd2e473d47c78
Philippe Gross <p.gross@4teamwork.ch>
2010-11-16 16:39:22 +0100

    updated setup.py (ftw standards)

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open('transmogrify/dexterity/version.txt').read().strip()
+maintainer = 'Philippe Gross'
 
 setup(name='transmogrify.dexterity',
       version=version,
-      description="",
+      description="Transmogrifier dexterity updateder section (Maintainer: %s)" % maintainer,
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -14,10 +15,11 @@ setup(name='transmogrify.dexterity',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='',
-      author_email='',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
       url='',
-      license='GPL',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['transmogrify'],
       include_package_data=True,
