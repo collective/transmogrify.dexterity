dfa719700016b3a775fe1f18c4f816da7173dc92
Jonas Baumann <jone@jone.ch>
2015-09-30 15:19:55 +0200

    Back to development: 1.6.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '1.6.1'
+version = '1.6.2.dev0'
 
 desc = 'A transmogrifier blueprint for updating dexterity objects'
 long_desc = open('README.rst').read() + '\n\n' + open('CHANGES.rst').read()
