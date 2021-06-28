909e3898e590b1506f2189052c9635d489f4efea
wesleybl <wesleybl@gmail.com>
2021-06-28 11:19:18 -0300

    Update package structure

---
+++ CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+Contributors
+============
+
+- 4teamwork AG, info@4teamwork.ch
+- Wesley Barroso, wesleybl@gmail.com
--- setup.py
+++ setup.py
@@ -1,43 +1,51 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
+"""Installer for the transmogrify.dexterity package."""
+
 from setuptools import find_packages
+from setuptools import setup
 
-version = '1.6.5.dev0'
 
-desc = 'A transmogrifier blueprint for updating dexterity objects'
-long_desc = open('README.rst').read() + '\n\n' + open('CHANGES.rst').read()
-tests_require = [
-    'ftw.builder',
-    'plone.app.intid',
-    'plone.app.testing',
-    'plone.app.dexterity',
-    'plone.namedfile[blobs]',
-    'plone.directives.form',
-    'plone.formwidget.contenttree',
-    'z3c.relationfield',
-    'unittest2',
-]
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+])
 
 
 setup(
     name='transmogrify.dexterity',
-    version=version,
-    description=desc,
-    long_description=long_desc,
+    version='1.6.5.dev0',
+    description="A transmogrifier blueprint for updating dexterity objects",
+    long_description=long_description,
+    # Get more from https://pypi.org/classifiers/
     classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='',
+    keywords='Python Plone CMS',
     author='4teamwork AG',
-    author_email='mailto:info@4teamwork.ch',
-    url='https://github.com/collective/transmogrify.dexterity/',
-    license='GPL2',
-    packages=find_packages(exclude=['ez_setup']),
+    author_email='info@4teamwork.ch',
+    url='https://github.com/collective/transmogrify.dexterity',
+    project_urls={
+        'PyPI': 'https://pypi.python.org/pypi/transmogrify.dexterity',
+        'Source': 'https://github.com/collective/transmogrify.dexterity',
+        'Tracker': 'https://github.com/collective/transmogrify.dexterity/issues',
+        # 'Documentation': 'https://transmogrify.dexterity.readthedocs.io/en/latest/',
+    },
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
     namespace_packages=['transmogrify'],
+    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
+    python_requires="==2.7",
     install_requires=[
-        'setuptools',
         'collective.transmogrifier',
         'plone.app.textfield',
         'plone.app.transmogrifier',
@@ -45,12 +53,26 @@ setup(
         'plone.namedfile',
         'plone.supermodel',
         'plone.uuid',
+        'setuptools',
         'z3c.form',
     ],
-    tests_require=tests_require,
-    extras_require=dict(tests=tests_require),
+    extras_require={
+        'test': [
+            'ftw.builder',
+            'plone.app.dexterity',
+            'plone.app.intid',
+            'plone.app.testing',
+            'plone.directives.form',
+            'plone.formwidget.contenttree',
+            'plone.namedfile[blobs]',
+            'unittest2',
+            'z3c.relationfield',
+        ],
+    },
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
+    [console_scripts]
+    update_locale = transmogrify.dexterity.locales.update:update_locale
     """,
 )
