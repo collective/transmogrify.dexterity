7e90169008c92f72bf296e70990298f44bd65e4f
wesleybl <wesleybl@gmail.com>
2021-06-28 11:06:47 -0300

    Revert "python 3 fixes"
    This reverts commit 36c1984318f8a3336bd36250ee12ec344d129651.

--- transmogrify/dexterity/schemaupdater.py
+++ transmogrify/dexterity/schemaupdater.py
@@ -12,7 +12,7 @@ from zope.component import getMultiAdapter
 from zope.component import queryMultiAdapter
 from zope.component.hooks import getSite
 from zope.event import notify
-from zope.interface import provider
+from zope.interface import classProvides
 from zope.interface import implementer
 from zope.lifecycleevent import ObjectModifiedEvent
 from zope.schema import getFieldsInOrder
@@ -23,8 +23,8 @@ _marker = object()
 
 
 @implementer(ISection)
-@provider(ISectionBlueprint)
 class DexterityUpdateSection(object):
+    classProvides(ISectionBlueprint)
 
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
