42689f09006b37042d6de5ff219a3e6d7c20c548
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2021-09-28 02:15:37 -0400

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="transmogrify.dexterity",
-    version="2.0.0",
+    version="2.0.1.dev0",
     description="A transmogrifier blueprint for updating dexterity objects",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
